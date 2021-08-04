6f51b94c6cab4c208f6c2f71ea937f232a3dbdde
Seanathan10 <38306929+Seanathan10@users.noreply.github.com>
2021-08-03 23:32:56 -0700

    Update

--- functions.js
+++ functions.js
@@ -1,7 +1,46 @@
 function CelsiusSelected() {
-    alert( "C" );
+    // alert( "C" );
+
+    document.getElementById( "ButtonC" ).style.color = "black";
+    document.getElementById( "ButtonC" ).style.backgroundColor = "green";
+
+    document.getElementById( "ButtonF" ).style.color = null;
+    document.getElementById( "ButtonF" ).style.backgroundColor = null;
+
+    ConvertToC();
   }
 
 function FahrenheitSelected() {
-    alert( "F" );
+    // alert( "F" );
+
+    document.getElementById( "ButtonF" ).style.color = "black";
+    document.getElementById( "ButtonF" ).style.backgroundColor = "green";
+
+    document.getElementById( "ButtonC" ).style.color = null;
+    document.getElementById( "ButtonC" ).style.backgroundColor = null;
+
+    ConvertToF();
   }
+
+function ConvertToC() {
+  // alert( "ConvertToC" );
+  
+  var InputValue = document.getElementById( "InputValueBox" ).value;
+
+  var FinalValue = InputValue - 32;
+  
+  FinalValue = FinalValue * ( 5 / 9 );
+
+  document.getElementById( "OutputValueBox" ).value = FinalValue.toFixed( 1 ) + " °C";
+}
+
+function ConvertToF() {
+  // alert( "ConvertToF" );
+
+  var InputValue = document.getElementById( "InputValueBox" ).value;
+
+  var FinalValue = InputValue * 1.8
+  FinalValue += 32;
+
+  document.getElementById( "OutputValueBox" ).value = FinalValue.toFixed( 1 ) + " °F";
+}
